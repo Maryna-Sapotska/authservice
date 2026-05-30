2cdea5191564bb2d3f43dd8432e8710b428ef94d
Maryna Sapotska <snaczkaya@mail.ru>
2026-05-30 14:36:36 +0300

    fix: add entity package

---
+++ src/main/java/com/innowise/authservice/entity/Role.java
@@ -0,0 +1,6 @@
+package com.innowise.authservice.entity;
+
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_USER
+}
---
+++ src/main/java/com/innowise/authservice/entity/UserCredentials.java
@@ -0,0 +1,73 @@
+package com.innowise.authservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users_credentials")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCredentials implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    private boolean active = true;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return active;
+    }
+}
