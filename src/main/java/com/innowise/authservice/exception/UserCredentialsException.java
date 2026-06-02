52d9a1d0cc6a3e7fb2625587c06b720ddb7393a2
Maryna Sapotska <snaczkaya@mail.ru>
2026-06-02 19:42:07 +0300

    refactor: deleted unused fields

--- src/main/java/com/innowise/authservice/exception/UserCredentialsException.java
+++ src/main/java/com/innowise/authservice/exception/UserCredentialsException.java
@@ -1,18 +1,9 @@
 package com.innowise.authservice.exception;
 
 public class UserCredentialsException extends RuntimeException {
-    public UserCredentialsException() {
 
-    }
     public UserCredentialsException(String message) {
         super(message);
     }
 
-    public UserCredentialsException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public  UserCredentialsException(Throwable cause) {
-        super(cause);
-    }
 }
